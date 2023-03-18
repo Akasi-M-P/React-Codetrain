5d88a0607c4fd18aa9e0d4371e7d7baf3519ebf4
Mba96 <mbajameel90@gmail.com>
2023-03-18 23:19:16 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <div className="header">
+      <h1>REACT WORKS !!!</h1>
+    </div>
+
+    <div className="codetrain">
+      <img src="./images/Codetrain.png" alt="" />
+
+      <h2>...is awesome.</h2>
+    </div>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
