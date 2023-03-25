01d916f1b26b8d570f1cb515e99a685e770319e8
Mba96 <mbajameel90@gmail.com>
2023-03-25 22:47:42 +0000

    class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+// export default App;
+
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
